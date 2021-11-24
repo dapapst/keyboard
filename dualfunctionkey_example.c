ae067bae61719147d25582dae31dab70622e4fc4
dapapst <davidbosse@gmx.net>
2021-11-24 10:52:45 +0100

    Update dualfunctionkey_example.c

--- dualfunctionkey_example.c
+++ dualfunctionkey_example.c
@@ -2,23 +2,31 @@ static uint16_t last_key_timer;
 uint16_t last_key_pressed = false;
 bool return_result = true;
 uint8_t mod_state;
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    
+    // if return result == true, process the key normal without any modifications
     return_result = true;
+    // get modifications like alt,shift,ctrl
     mod_state = get_mods();
     if(record->event.pressed) // it's pressed
        last_key_timer = timer_read();
+
     switch (keycode) {
         case KC_BSPACE:
-            if (mod_state & MOD_MASK_SHIFT) {
-                return_result = true;
+            // if shift + backspace behave normal
+            if (mod_state & MOD_MASK_SHIFT) 
                 break;
-            }
+                
             return_result = false;
-            if(record->event.pressed) // it's pressed
+            if(record->event.pressed) // it's pressed activate layer 2
                 layer_on(2);
-            else
+            else // it is released, deactivate layer 2
                 layer_off(2);
-            // if it is only a tap(less than 200ms) and the layer wasnt used and only on key up, use normal backspace
+                
+            // Decide if we have to trigger backspace or not
+            // if it is only a tap(less than 200ms) and the layer wasnt used and only on key up, trigger backspace
             if(timer_elapsed(last_key_timer) < 200 && last_key_pressed == KC_BSPACE && !record->event.pressed)
                 tap_code(KC_BSPACE);
             break;
