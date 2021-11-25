f669579aa383887db68b34fff8ab1b866751f840
dapapst <davidbosse@gmx.net>
2021-11-25 13:34:50 +0100

    Add files via upload

--- layout/keymap.c
+++ layout/keymap.c
@@ -1,7 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-#include "print.h"
-#include "time.h"
 #include "keymap_german.h"
 #include "keymap_nordic.h"
 #include "keymap_french.h"
@@ -49,7 +47,6 @@
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   ST_MACRO_0,
-  ST_MACRO_1,
 };
 
 
@@ -61,35 +58,35 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TG(3),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Z,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           ST_MACRO_0,     KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_Y,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_LCTRL,       KC_LSHIFT,      KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_BSPACE,    KC_LALT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TG(3),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Z,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           ST_MACRO_0,     KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_Y,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_LCTRL,       KC_LSHIFT,      KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, TD(DANCE_0),    KC_LALT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     LT(1,KC_SPACE), KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
-    RESET,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_ESCAPE,      KC_CIRC,        KC_TILD,        LSFT(KC_TAB),   KC_TAB,         KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         KC_CAPSLOCK,    KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_LCTRL,       KC_LALT,        KC_LSHIFT,      KC_ENTER,       KC_DELETE,      KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_PGDOWN,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_DELETE,      KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F10,         KC_F12,         KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    RESET,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_ESCAPE,      KC_CIRC,        KC_TILD,        LSFT(KC_TAB),   KC_TAB,         KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         KC_CAPSLOCK,    KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_LCTRL,       KC_LALT,        KC_LSHIFT,      KC_ENTER,       KC_DELETE,      KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_PGDOWN,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_DELETE,      KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F10,         KC_F12,         KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_LBRACKET,    KC_RBRACKET,    KC_LPRN,        KC_RPRN,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, TD(DANCE_1),    KC_EQUAL,       TD(DANCE_2),    KC_ASTR,        KC_PLUS,                                        KC_GRAVE,       KC_LCBR,        KC_RCBR,        KC_COLN,        KC_SCOLON,      KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_LBRACKET,    KC_RBRACKET,    KC_LPRN,        KC_RPRN,        KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
+    KC_TRANSPARENT, TD(DANCE_1),    KC_EQUAL,       TD(DANCE_2),    KC_ASTR,        KC_PLUS,                                        KC_GRAVE,       KC_LCBR,        KC_RCBR,        KC_COLN,        KC_SCOLON,      KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [3] = LAYOUT_moonlander(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_F11,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F6,                                          KC_TRANSPARENT, KC_Z,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    KC_MINUS,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_F5,                                                                          KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_LSHIFT,      KC_Y,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_LCTRL,       KC_LALT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F3,                                                                                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_F11,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F6,                                          KC_TRANSPARENT, KC_Z,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
+    KC_MINUS,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_F5,                                                                          KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_LSHIFT,      KC_Y,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_LCTRL,       KC_LALT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F3,                                                                                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_SPACE,       KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
@@ -99,9 +96,6 @@ extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void) {
   rgb_matrix_enable();
-    debug_enable=true;
-    debug_matrix=true;
-    debug_keyboard=true;
 }
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
@@ -147,12 +141,6 @@ void rgb_matrix_indicators_user(void) {
     case 3:
       set_layer_color(3);
       break;
-    case 4:
-      set_layer_color(4);
-      break;
-    case 6:
-      set_layer_color(6);
-      break;
    default:
     if (rgb_matrix_get_flags() == LED_FLAG_NONE)
       rgb_matrix_set_color_all(0, 0, 0);
@@ -160,141 +148,21 @@ void rgb_matrix_indicators_user(void) {
   }
 }
 
-
-
-static uint16_t last_key_timer;
-uint16_t last_key_pressed = false;
-bool process_key_default = true;
-uint8_t mod_state;
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    printf("last key pressed is %d \n", last_key_pressed);
+  switch (keycode) {
+    case ST_MACRO_0:
+    if (record->event.pressed) {
+      SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_SPACE));
 
-    // if return result == true, process the key normal without any modifications
-    process_key_default = true;
-    // get modifications like alt,shift,ctrl
-    printf("alt  mod %d \n", (get_mods() & MOD_BIT(KC_LALT)));
-
-    printf("shift mod %d \n",( get_mods() & MOD_BIT(KC_LSHIFT)));
-
-    printf("ctrl mod %d \n", (get_mods() & MOD_BIT(KC_LCTL)));
-    if(record->event.pressed) // it's pressed
-       last_key_timer = timer_read();
-
-    switch (keycode) {
-        case ST_MACRO_0:
-            if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_QUOTE) SS_DELAY(100) SS_TAP(X_SPACE));
-            }
-            break;
-        case ST_MACRO_1:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LALT(SS_TAP(X_S) ));
-            }
-            break;
-        case RGB_SLD:
-            if (record->event.pressed) {
-                rgblight_mode(1);
-            }
-            process_key_default = false;
-            break;
-        case KC_A:
-            if (get_mods() & MOD_BIT(KC_LALT)){
-                if(record->event.pressed){
-                    register_code(KC_RALT);
-                    register_code(KC_Q);
-                }else{
-                    unregister_code(KC_Q);
-                    unregister_code(KC_RALT);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_U:
-            if (get_mods() & MOD_BIT(KC_LALT)){
-                if(record->event.pressed){
-                    register_code(KC_RALT);
-                    register_code(KC_Y);
-                }else{
-                    unregister_code(KC_Y);
-                    unregister_code(KC_RALT);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_S:
-            if (get_mods() & MOD_BIT(KC_LALT)){
-                if(record->event.pressed){
-                    register_code(KC_RALT);
-                    register_code(KC_S);
-                }else{
-                    unregister_code(KC_S);
-                    unregister_code(KC_RALT);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_O:
-            if (get_mods() & MOD_BIT(KC_LALT)){
-                if(record->event.pressed){
-                    register_code(KC_RALT);
-                    register_code(KC_P);
-                }else{
-                    unregister_code(KC_P);
-                    unregister_code(KC_RALT);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_E:
-            if (get_mods() & MOD_BIT(KC_LALT)){
-                if(record->event.pressed){
-                    register_code(KC_RALT);
-                    register_code(KC_5);
-                }else{
-                    unregister_code(KC_5);
-                    unregister_code(KC_RALT);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_Y:
-            if (get_mods() & MOD_BIT(KC_LCTRL)){
-                if(record->event.pressed){
-                    register_code(KC_RCTL);
-                    register_code(KC_Z);
-
-                }else{
-                    unregister_code(KC_Z);
-                    unregister_code(KC_RCTL);
-                }
-                process_key_default = false;
-            }
-            break;
-        case KC_BSPACE:
-            // if shift + backspace behave normal
-            if (mod_state & MOD_MASK_SHIFT)
-                break;
-
-            process_key_default = false;
-            if(record->event.pressed) // it's pressed activate layer 2
-                layer_on(2);
-            else // it is released, deactivate layer 2
-                layer_off(2);
-
-            // Decide if we have to trigger backspace or not
-            // if it is only a tap(less than 200ms) and the layer wasnt used and only on key up, trigger backspace
-            if(timer_elapsed(last_key_timer) < 200 && last_key_pressed == KC_BSPACE && !record->event.pressed)
-                tap_code(KC_BSPACE);
-            break;
-        default:
-            process_key_default = true;
-            break;
+    }
+    break;
+    case RGB_SLD:
+      if (record->event.pressed) {
+        rgblight_mode(1);
+      }
+      return false;
   }
-  printf("process_key_default is %d\n", process_key_default);
-  last_key_pressed = keycode;
-  return process_key_default;
+  return true;
 }
 
 typedef struct {
@@ -328,7 +196,43 @@ uint8_t dance_step(qk_tap_dance_state_t *state) {
 }
 
 
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
 
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(KC_BSPACE);
+        tap_code16(KC_BSPACE);
+        tap_code16(KC_BSPACE);
+    }
+    if(state->count > 3) {
+        tap_code16(KC_BSPACE);
+    }
+}
+
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[0].step = dance_step(state);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: register_code16(KC_BSPACE); break;
+        case SINGLE_HOLD: layer_on(2); break;
+        case DOUBLE_TAP: register_code16(KC_BSPACE); register_code16(KC_BSPACE); break;
+        case DOUBLE_HOLD: register_code16(KC_BSPACE); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_BSPACE); register_code16(KC_BSPACE);
+    }
+}
+
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: unregister_code16(KC_BSPACE); break;
+        case SINGLE_HOLD: layer_off(2); break;
+        case DOUBLE_TAP: unregister_code16(KC_BSPACE); break;
+        case DOUBLE_HOLD: unregister_code16(KC_BSPACE); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_BSPACE); break;
+    }
+    dance_state[0].step = 0;
+}
 void on_dance_1(qk_tap_dance_state_t *state, void *user_data);
 void dance_1_finished(qk_tap_dance_state_t *state, void *user_data);
 void dance_1_reset(qk_tap_dance_state_t *state, void *user_data);
@@ -401,6 +305,7 @@ void dance_2_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
+        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
         [DANCE_2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
 };
--- layout/rules.mk
+++ layout/rules.mk
@@ -1,6 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = yes
+CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
 TAP_DANCE_ENABLE = yes
